441f2c8c6c40f46fb56f898396fd29a8a9c66c38
Viktor Sheverdin <viktor.sheverdin@houle.ca>
2021-02-23 17:09:31 -0800

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from bs4 import BeautifulSoup
-with open("website.html", encoding="utf8") as file:
-    contents = file.read()
+import requests
+response = requests.get("https://news.ycombinator.com/")
+yc_web_page = response.text
 
-soup = BeautifulSoup(contents, "html.parser")
-# print(soup.title.string)
-
-all_anchor_tags = soup.find_all(name="a")
-
-for tag in all_anchor_tags:
-    print(tag.get("href"))
+soup = BeautifulSoup(yc_web_page, "html.parser")
+links = soup.find_all(name='a', class_="storylink")
+upvote = soup.find(name="span", class_="score").getText()
+print(f"""
+Article: {links[0].string}
+\nlink: {links[0].get('href')}
+\nupvote: {upvote}
+""")
+# print()
+# print(f"Article: {links[0]}")
